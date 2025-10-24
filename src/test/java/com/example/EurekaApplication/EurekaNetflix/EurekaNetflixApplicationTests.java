a099a5391a4564f9008db504536ad20223ec731c
Kumar Bishwajeet <bishwajeetk23@gmail.com>
2025-10-25 01:45:17 +0530

    Initialize Service Discovery for Ecommerse project

---
+++ src/test/java/com/example/EurekaApplication/EurekaNetflix/EurekaNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaApplication.EurekaNetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
